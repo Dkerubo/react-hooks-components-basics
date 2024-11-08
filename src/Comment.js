8047cf6c7d3dbd29850dea2d6dd156f452f7b44e
root <root@Kerry>
2024-11-08 20:16:06 +0300

    practice

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <div>Naturally, I agree with this article. Hey, when you want to put this component on the DOM, here is what it should become!</div>;
 }
 
 export default Comment;
